6090d208cfefc53341aea266dea304b4a925fb42
Your Name <neo_tran@outlook.com>
2024-07-13 01:02:12 -0600

    feat: implement storybook

---
+++ src/stories/atomic.button.stories.js
@@ -0,0 +1,40 @@
+import { html } from 'lit';
+import '../atomic.button.component';
+
+export default {
+  title: 'AtomicButtonComponent',
+};
+
+const Template = (args) => html`
+  <atomic-button
+    textalign=${args.textalign}
+    height=${args.height}
+    color=${args.color}
+    label=${args.label}
+    icon=${args.icon}
+    badge=${args.badge}
+    link=${args.link}
+    disabled=${args.disabled}
+    outlined=${args.outlined}
+  ></atomic-button>
+`;
+
+export const Default = Template.bind({});
+Default.args = {
+  textalign: 'center',
+  height: '2',
+  color: 'default',
+  label: 'Click Me',
+  icon: '',
+  badge: '',
+  link: false,
+  disabled: false,
+  outlined: false,
+};
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+  ...Default.args,
+  outlined: true,
+  label: 'Outlined Button',
+};
